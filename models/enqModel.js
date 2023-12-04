3cd320ef5c4c0b57e4b6fbd2d7ea51ea9b0fc799
EOluwaseun <oluwaseunajibowo@gmail.com>
2023-12-04 01:03:05 +0100

    enquiry schema modidified

--- models/enqModel.js
+++ models/enqModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const enquirySchema = new mongoose.Schema(
   {
-    nam: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true },
     email: { type: String, required: true },
     mobile: { type: String, required: true },
     comment: { type: String, required: true },
